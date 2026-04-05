119fa43af488bfe5d1dfb6d90625d247907a70da
Yusuf YILDIRIM <yusuf@tinythin.gs>
2026-04-05 13:28:20 +0200

    feat: add focus-nav package to the project

---
+++ packages/focus-nav/README.md
@@ -0,0 +1,11 @@
+## Spatial Navigation
+
+This package is a fork of Norigin Media's [react-spatial-navigation](https://github.com/NoriginMedia/Norigin-Spatial-Navigation) library. It exdends the original library with additional features, fixes and performance improvements.
+
+## Key Changes
+
+Will be documented here.
+
+### Additional Features
+
+Will be documented here.
---
+++ packages/focus-nav/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.ts',
+  target: ['web', 'es5'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  externals: ['react', /^lodash(\/.+)?$/],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    clean: true,
+    globalObject: 'this'
+  }
+};
